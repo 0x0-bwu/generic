5aec9e6ad1a32d91d3b9d9015bdee7aac4ed45ff
Draaaaaaven <350240702@qq.com>
2021-11-22 21:59:16 +0800

    update

--- geometry/Tetrahedralization.hpp
+++ geometry/Tetrahedralization.hpp
@@ -5,11 +5,15 @@
 #include <vector>
 #include <array>
 #include <list>
+
+#ifdef BOOST_SERIALIZATION_SUPPORT
+#include "Serialization.hpp"
+#endif
+
 namespace generic {
 namespace geometry {
 namespace tet {
 
-using IndexEdge = topology::UndirectedIndexEdge;
 using IndexEle4 = std::array<size_t,  4>;
 using IndexEle10 = std::array<size_t, 10>;
 
@@ -43,6 +47,121 @@ using index_t = topology::index_t;
 using PosIdx = index_t;
 using VerIdx = index_t;
 using TetIdx = index_t;
+
+template <typename point_t>
+using PointVec = std::vector<point_t>;
+using VertexVec = std::vector<IndexVertex>;
+using TetIdxVec = std::vector<TetIdx>;
+using VerIdxSet = std::unordered_set<VerIdx>;
+using TetIdxSet = std::unordered_set<TetIdx>;
+using TetrahedronVec = std::vector<IndexTetrahedron>;
+using IndexEdge = topology::UndirectedIndexEdge;
+using IndexEdgeHash = topology::UndirectedIndexEdgeHash;
+using IndexEdgeCompare = topology::UndirectedIndexEdgeCompare;
+
+using topology::noIndex;
+inline static constexpr VerIdx noVertex = noIndex;
+inline static constexpr TetIdx noNeighbor = noIndex;
+
+struct IndexVertex
+{
+    PosIdx index;
+    TetIdxSet tetrahedrons;
+
+    void RemoveTetrahedron(TetIdx it) { tetrahedrons.erase(it); }
+
+    bool operator != (const IndexVertex & v) const { return index != v.index || tetrahedrons != v.tetrahedrons; }
+    bool operator == (const IndexVertex & v) const { return !(*this != v); }
+
+    friend std::ostream & operator<< (std::ostream & os, const IndexVertex & v)
+    {
+        os << "point index: " << v.index << ", tetrahedrons index:";
+        std::for_each(v.tetrahedrons.begin(), v.tetrahedrons.end(), [&](TetIdx i) mutable { os << " " << i; });
+        return os;
+    }
+
+    void Clear() { tetrahedrons.clear(); }
+
+    static bool isShareEdge(const IndexVertex & a, const IndexVertex & b)
+    {
+        if(a.tetrahedrons.size() && b.tetrahedrons.size()){
+            for(TetIdx it : a.tetrahedrons){
+                if(b.tetrahedrons.count(it)) return true;
+            }
+        }
+        return false;
+    }
+
+#ifdef BOOST_SERIALIZATION_SUPPORT
+private:
+    friend class boost::serialization::access;
+    template <typename Archive>
+    void serialize(Archive & ar, const unsigned int)
+    {
+        ar & index;
+        ar & tetrahedrons;
+    }
+#endif
+};
+
+struct IndexTetrahedron
+{
+    using IdxEdge = IndexEdge;
+    std::array<VerIdx, 4> vertices = {noVertex, noVertex, noVertex, noVertex};
+    std::array<TetIdx, 4> neighbors = {noNeighbor, noNeighbor, noNeighbor, noNeighbor};
+
+    void Reset()
+    {
+        vertices[0] = vertices[1] = vertices[2] = vertices[3] = noVertex;
+        neighbors[0] = neighbors[1] = neighbors[2] = neighbors[3] = noNeighbor;
+    }
+
+#ifdef BOOST_SERIALIZATION_SUPPORT
+private:
+    friend class boost::serialization::access;
+    template <typename Archive>
+    void serialize(Archive & ar, const unsigned int)
+    {
+        ar & vertices;
+        ar & neighbors;
+    }
+#endif
+};
+
+template <typename point_t>
+struct Tetrahedralization
+{
+    using Edge = IndexEdge;
+    using Vertex = IndexVertex;
+    using Tetrahedron = IndexTetrahedron;
+    using EdgeSet = topology::UndirectedIndexEdgeSet;
+
+    EdgeSet fixedEdges;
+    VertexVec vertices;
+    PointVec<point_t> points;
+    TetrahedronVec tetrahedrons;
+    
+    void Clear()
+    {
+        points.clear();
+        vertices.clear();
+        fixedEdges.clear();
+        tetrahedrons.clear();
+    }
+
+#ifdef BOOST_SERIALIZATION_SUPPORT
+private:
+    friend class boost::serialization::access;
+    template <typename Archive>
+    void serialize(Archive & ar, const unsigned int)
+    {
+        ar & tetrahedrons;
+        ar & fixedEdges;
+        ar & vertices;
+        ar & points;
+    }
+#endif
+};
 
 }//namespace tet
 }//namespace geometry
