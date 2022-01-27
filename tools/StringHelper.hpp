218784bd0666b2dda2f82f1b90d85aee36e50bd1
Draaaaaaven <350240702@qq.com>
2022-01-27 17:30:23 +0800

    replace with boost string algorithm

--- tools/StringHelper.hpp
+++ tools/StringHelper.hpp
@@ -1,19 +1,27 @@
 #ifndef GENERIC_STR_STRINGHELPER_HPP
 #define GENERIC_STR_STRINGHELPER_HPP
+#include <boost/algorithm/string.hpp>
 #include <string>
 namespace generic {
 namespace str {
 
+using CaseSensitive = std::true_type;
+using CaseInsensitive = std::false_type;
+
+template <typename C = CaseSensitive> 
 inline bool StartsWith(const std::string & s, const std::string & prefix)
 {
-    if(prefix.size() > s.size()) return false;
-    return s.rfind(prefix, 0) == 0;
+    if constexpr (C::value)
+        return boost::starts_with(s, prefix);
+    else return boost::istarts_with(s, prefix);
 }
 
+template <typename C = CaseSensitive> 
 inline bool EndsWith(const std::string & s, const std::string & suffix)
 {
-    if (suffix.size() > s.size()) return false;
-    return std::equal(suffix.rbegin(), suffix.rend(), s.rbegin());
+    if constexpr (C::value)
+        return boost::ends_with(s, suffix);
+    else return boost::iends_with(s, suffix);
 }
 
 }//str
