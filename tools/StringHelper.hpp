1b890240394a7038b24dfc06e808fabdc0e18e28
Draaaaaaven <350240702@qq.com>
2022-02-16 14:54:06 +0800

    add string compare

--- tools/StringHelper.hpp
+++ tools/StringHelper.hpp
@@ -24,6 +24,14 @@ inline bool EndsWith(const std::string & s, const std::string & suffix)
     else return boost::iends_with(s, suffix);
 }
 
+template <typename C = CaseSensitive>
+inline bool Equals(const std::string & s1, const std::string & s2)
+{
+    if constexpr (C::value)
+        return boost::equals(s1, s2);
+    else return boost::iequals(s1, s2);
+}
+
 }//str
 }//generic
 #endif//GENERIC_STR_STRINGHELPER_HPP
