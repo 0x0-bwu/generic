697dfdc73413c4555284f74d1bbaf87e011b2746
bwu <350240702@qq.com>
2022-08-16 09:49:48 +0800

    performance enhance

--- thread/TaskFlow.hpp
+++ thread/TaskFlow.hpp
@@ -8,14 +8,11 @@
 #ifndef GENERIC_THREAD_TASKFLOW_HPP
 #define GENERIC_THREAD_TASKFLOW_HPP
 #include "ThreadPool.hpp"
-#include <boost/graph/topological_sort.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/graphviz.hpp>
-#include <atomic>
-#include <vector>
-#include <deque>
-#include <list>
+#include <condition_variable>
+#include <unordered_map>
+#include <unordered_set>
+#include <optional>
+
 namespace generic {
 namespace thread {
 ///@brief namespace of task flow library
@@ -33,9 +30,9 @@ class TaskNode
     friend Executor;
     friend Dispatcher;
 public:
-    ///@brief constructs a task node with node id and funtion object to be executed
+    ///@brief constructs a task node with funtion object and label(optional) to be executed
     template<typename FunctionType>
-    TaskNode(size_t id, FunctionType && func);
+    TaskNode(FunctionType && func, std::optional<std::string> label);
 
     ///@brief add precede dependency for this node, input tasks will be executed after this task
     template <typename... Args>
@@ -49,78 +46,71 @@ public:
     void Success(){}
 
 private:
-    size_t m_id;
-    std::list<TaskNode * > m_successors;
-    std::list<TaskNode * > m_dependents;
+    void PrecedeImp(TaskNode * task);
+    void SuccessImp(TaskNode * task);
+
+private:
+    std::unordered_set<TaskNode * > m_successors;
+    std::unordered_set<TaskNode * > m_dependents;
     std::unique_ptr<FunctionWrapper > m_work;
+    std::optional<std::string> m_label;
+    std::atomic<size_t> m_depCount{0};
 };
 
 template<typename FunctionType>
-inline TaskNode::TaskNode(size_t id, FunctionType && func)
- : m_id(id), m_work(new FunctionWrapper(std::move(func))) {}
+inline TaskNode::TaskNode(FunctionType && func, std::optional<std::string> label)
+ : m_work(new FunctionWrapper(std::move(func))), m_label(std::move(label)) {}
 
 template <typename... Args>
 inline void TaskNode::Precede(TaskNode * task, Args && ...args)
 {
-    m_successors.push_back(task);
+    this->PrecedeImp(task);
+    task->SuccessImp(this);
     Precede(std::forward<Args &&>(args)...);
 }
 
 template <typename... Args>
 inline void TaskNode::Success(TaskNode * task, Args && ...args)
 {
-    m_dependents.push_back(task);
+    this->SuccessImp(task);
+    task->PrecedeImp(this);
     Success(std::forward<Args &&>(args)...);
+}
+
+inline void TaskNode::PrecedeImp(TaskNode * task)
+{
+    m_successors.emplace(task);
+}
+    
+inline void TaskNode::SuccessImp(TaskNode * task)
+{
+    if(m_dependents.emplace(task).second)
+        m_depCount += 1;
 }
 
 ///@brief task flow class for registering and managing tasks
 class TaskFlow
 {
-    using TaskGraph = boost::adjacency_list<
-                      boost::vecS,
-                      boost::vecS,
-                      boost::bidirectionalS,
-                      boost::property<boost::vertex_index_t, size_t >
-                      >;
-    using Vertex = boost::graph_traits<TaskGraph>::vertex_descriptor;
-
-    class TaskLabelWriter
-    {
-    public:
-        TaskLabelWriter(const std::unordered_map<size_t, std::string> & labels) : m_labels(labels){}
-
-        void operator() (std::ostream & out, const Vertex & v) const
-        {
-            if(m_labels.count(v)){ out << "[label=\"" << m_labels.at(v) << "\"]"; }
-            else { out << "[label=\"" << "task" << std::to_string(v) << "\"]"; }
-        }
-    private:
-        const std::unordered_map<size_t, std::string> & m_labels;
-    };
-
     friend Executor;
     friend Dispatcher;
 public:
-    TaskFlow(){}
-    ~TaskFlow(){}
+    TaskFlow() = default;
+    ~TaskFlow() = default;
     TaskFlow(TaskFlow & other) = delete;
     TaskFlow(const TaskFlow & other) = delete;
     TaskFlow & operator= (TaskFlow & other) = delete;
-
-    ///@brief accesses task node by index
-    TaskNode& operator[] (size_t i) {return *m_tasks[i];}
 
     /**
      * @brief submit a task and get the internal task node
      * @tparam FunctionType could be one of std::bind, std::function or lambda expression
      * @param f Task function object
      * @param label task label name
-     * @return a pair with internal task node and std::future of the FunctionType result
+     * @return a pair with internal task node and std::shared_future of the FunctionType result
      */
     template <typename FunctionType>
     std::pair<TaskNode *,
-    std::future<typename std::result_of<FunctionType()>::type> >
-    Submit(FunctionType && f, std::string label = std::string());
+    std::shared_future<typename std::result_of<FunctionType()>::type> >
+    Submit(FunctionType && f, std::optional<std::string> label = {});
 
     /**
      * @brief emplace a task and get the internal task node
@@ -131,106 +121,62 @@ public:
      * @return TaskNode* internal task node
      */
     template <typename FunctionType>
-    TaskNode * Emplace(FunctionType && f, std::string label = std::string());
+    TaskNode * Emplace(FunctionType && f, std::optional<std::string> label = {});
 
-    ///@brief build a direct task flow graph based on added tasks dependency
-    void BuildTaskGraph();
     ///@brief print *.dot format task graph to out stream
     void PrintTaskGraph(std::ostream & out);
-private:
-    void TopologicalSort();
-    std::list<TaskNode * > Successors(TaskNode & node);
-    std::list<TaskNode * > Dependents(TaskNode & node);
 
 private:
-    std::deque<Vertex> m_sequence;
-    std::unique_ptr<TaskGraph> m_graph;
+    const std::unordered_set<TaskNode * > & Successors(TaskNode & node) const { return node.m_successors; }
+    const std::unordered_set<TaskNode * > & Dependents(TaskNode & node) const { return node.m_dependents; }
+
+private:
+    bool m_executed = false;
     std::vector<std::unique_ptr<TaskNode> > m_tasks;
-    std::unordered_map<size_t, std::string> m_taskLabels;
-    std::vector<std::unique_ptr<std::atomic<size_t> > > m_dependents;
 };
 
 template <typename FunctionType>
 std::pair<TaskNode *,
-std::future<typename std::result_of<FunctionType()>::type> >
-inline TaskFlow::Submit(FunctionType && f, std::string label)
+std::shared_future<typename std::result_of<FunctionType()>::type> >
+inline TaskFlow::Submit(FunctionType && f, std::optional<std::string> label)
 {
-    using result_type = typename std::result_of<FunctionType()>::type;
-    std::packaged_task<result_type()> task(std::move(f));
-    std::future<result_type> res(task.get_future());
+    using ResultType = typename std::result_of<FunctionType()>::type;
+    std::packaged_task<ResultType()> task(std::move(f));
+    std::shared_future<ResultType> res(task.get_future());
 
-    size_t id = m_tasks.size();
-    m_tasks.emplace_back(new TaskNode(id, std::move(task)));
-
-    if(label.empty()) label = "task" + std::to_string(id);
-    m_taskLabels.insert(std::make_pair(id, label));
+    m_tasks.emplace_back(new TaskNode(std::move(task), std::move(label)));
     return std::make_pair(m_tasks.back().get(), std::move(res));
 }
 
 template <typename FunctionType>
-inline TaskNode *  TaskFlow::Emplace(FunctionType && f, std::string label)
+inline TaskNode *  TaskFlow::Emplace(FunctionType && f, std::optional<std::string> label)
 {
-    size_t id = m_tasks.size();
-    m_tasks.emplace_back(new TaskNode(id, std::move(f)));
-
-    if(label.empty()) label = "task" + std::to_string(id);
-    m_taskLabels.insert(std::make_pair(id, label));
+    m_tasks.emplace_back(new TaskNode(std::move(f), std::move(label)));
     return m_tasks.back().get();
-}
-
-inline std::list<TaskNode * > TaskFlow::Successors(TaskNode & node)
-{
-    if(nullptr == m_graph) BuildTaskGraph();
-
-    std::list<TaskNode * > successors;
-    auto ee = boost::out_edges(node.m_id, *m_graph);
-    for(auto e = ee.first; e != ee.second; ++e){
-        Vertex v = boost::target(*e, *m_graph);
-        successors.push_back(&(*m_tasks[v]));
-    }
-    return successors;
-}
-
-inline std::list<TaskNode * > TaskFlow::Dependents(TaskNode & node)
-{
-    if(nullptr == m_graph) BuildTaskGraph();
-
-    std::list<TaskNode * > dependents;
-    auto ee = boost::in_edges(node.m_id, *m_graph);
-    for(auto e = ee.first; e != ee.second; ++e){
-        Vertex v = boost::source(*e, *m_graph);
-        dependents.push_back(&(*m_tasks[v]));
-    }
-    return dependents;
-}
-
-inline void TaskFlow::BuildTaskGraph()
-{
-    size_t taskSize = m_tasks.size();
-    m_graph.reset(new TaskGraph(taskSize));
-    for(size_t i = 0; i < taskSize; ++i){
-        auto & node = m_tasks[i];
-        for(TaskNode * successor : node->m_successors)
-            boost::add_edge(i, successor->m_id, *m_graph);
-        for(TaskNode * dependent : node->m_dependents)
-            boost::add_edge(dependent->m_id, i, *m_graph);
-    }
-
-    m_dependents.resize(taskSize);
-    for(size_t i = 0; i < taskSize; ++i)
-        m_dependents[i].reset(new std::atomic<size_t>(boost::in_degree(i, *m_graph)));
 }
 
 inline void TaskFlow::PrintTaskGraph(std::ostream & out)
 {
-    if(nullptr == m_graph) BuildTaskGraph();
-    boost::write_graphviz(out, *m_graph, TaskLabelWriter(m_taskLabels));
-}
+    if(out.bad()) return;
 
-inline void TaskFlow::TopologicalSort()
-{
-    m_sequence.clear();
-    boost::topological_sort(*m_graph, std::front_inserter(m_sequence));
+    const char sp(' ');
+    const char eol('\n');
+    std::string indent(2, sp);
+    out << "digraph g {" << eol;
+    
+    std::unordered_map<TaskNode *, size_t> idMap;
+    for(size_t i = 0; i < m_tasks.size(); ++i) {
+        idMap.emplace(m_tasks[i].get(), i);
+        out << indent << 'n' << std::to_string(i) << "[label=\"" << m_tasks[i]->m_label.value_or("task" + std::to_string(i)) << "\"];" << eol;
+    }
+
+    for(size_t i = 0; i < m_tasks.size(); ++i) {
+        for(auto * dep : m_tasks[i]->m_successors) {
+            out << indent << 'n' << std::to_string(i) << sp << "->" << sp << 'n' << std::to_string(idMap.at(dep)) << ';' << eol;
+        }
+    }
+
+    out << '}' << eol;
 }
 
 ///@brief dispatcher class that dispatch tasks based on dependency
@@ -242,39 +188,41 @@ public:
     
     void Dispatch(size_t threads);
 private:
-    void DispatchOne(TaskNode & node, ThreadPool * pool);
+    void DispatchOne(TaskNode * node, ThreadPool * pool);
 private:
     TaskFlow & m_flow;
-    std::atomic<size_t> m_nWait;
+    mutable std::mutex m_mutex;
+    std::condition_variable m_cond;
+    std::atomic<size_t> m_nTasks{0};
 };
 
 inline void Dispatcher::Dispatch(size_t threads)
 {
     ThreadPool pool(threads);
-    m_nWait.store(m_flow.m_sequence.size());
-
-    std::list<TaskNode *> nodes;
-    for(auto v : m_flow.m_sequence) {
-        auto dep = m_flow.m_dependents[v]->load();
-        if(0 == dep) nodes.emplace_back(&m_flow[v]);
+    m_nTasks.store(m_flow.m_tasks.size());
+    
+    std::vector<TaskNode *> tasks;
+    for(auto & task : m_flow.m_tasks) {
+        if(task->m_depCount == 0)
+            tasks.emplace_back(task.get());
     }
+
+    for(auto * task : tasks)
+        pool.Submit(std::bind(&Dispatcher::DispatchOne, this, task, &pool));
     
-    for(auto * node : nodes)
-        pool.Submit(std::bind(&Dispatcher::DispatchOne, this, std::ref(*node), &pool));
-    
-    while(m_nWait.load());
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_cond.wait(lock, [&]{ return m_nTasks.load() == 0; });
 }
 
-inline void Dispatcher::DispatchOne(TaskNode & node, ThreadPool * pool)
+inline void Dispatcher::DispatchOne(TaskNode * node, ThreadPool * pool)
 {
-    (*node.m_work)();
-    auto successors = m_flow.Successors(node);
-    for(auto * s : successors){
-        if(auto dep = m_flow.m_dependents[s->m_id]->fetch_sub(1); dep == 1)
-            pool->Submit(std::bind(&Dispatcher::DispatchOne, this, std::ref(*s), pool));
+    (*node->m_work)();
+    m_nTasks.fetch_sub(1);
+    for(auto * successor : node->m_successors) {
+        if(auto dep = successor->m_depCount.fetch_sub(1); dep == 1)
+            pool->Submit(std::bind(&Dispatcher::DispatchOne, this, successor, pool));
     }
-
-    m_nWait.fetch_sub(1);
+    m_cond.notify_one();
 }
 
 ///@brief executor class to run taskflow in parallel
@@ -288,23 +236,35 @@ public:
      * @brief execute input task flow `flow` parallelly in setting threads
      * @param[in] flow the take flow to be executed
      * @return true when task flow executed successfully
-     * @return false if any task has circular dependency 
+     * @return false if flow has been executed once or has circular dependency when the check enabled
      */
     bool Run(TaskFlow & flow);
+
+    ///@brief set if check task cyclic dependency before execute
+    void SetCheckCyclicDependency(bool check);
+
 private:
+    bool m_cycleCheck = true;
     size_t m_threads;
 };
 
 inline bool Executor::Run(TaskFlow & flow)
 {
-    flow.BuildTaskGraph();
-    try { flow.TopologicalSort(); }
-    catch (...) { return false; }
+    if(flow.m_executed) return false;
+    if(m_cycleCheck) {
+        //wbtest, todo
+    }
 
+    flow.m_executed = true;
     Dispatcher dispatcher(flow);
     dispatcher.Dispatch(m_threads);
     return true;
 }
+
+inline void Executor::SetCheckCyclicDependency(bool check)
+{
+    m_cycleCheck = check;
+} 
 
 }//namespace taskflow
 }//namespace thread
