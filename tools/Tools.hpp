91bf374f7cf17b04fa2487cecd3f55af78a37fe9
bwu <350240702@qq.com>
2022-08-25 16:52:33 +0800

    add accumulated timer

--- tools/Tools.hpp
+++ tools/Tools.hpp
@@ -121,6 +121,113 @@ private:
     Timer m_timer;
 };
 
+///@brief thread safe timer singleton, usually used to track total function execution time in threads
+class AccumulatedTimer
+{
+public:
+    class SubTimer
+    {
+    public:
+        explicit SubTimer(std::shared_ptr<AccumulatedTimer> master) : m_master(master)
+        {
+            m_start = std::chrono::steady_clock::now(); 
+        }
+        
+        ~SubTimer() { try { Stop(); } catch (...) {} }
+
+        ///@brief stop timing explicitly, or it will stop when destructing
+        void Stop()
+        {
+            if (not m_stop) {
+                m_stop = true;
+                auto end = std::chrono::steady_clock::now();
+                auto elapse = std::chrono::duration_cast<std::chrono::nanoseconds>(end - m_start);
+                m_master->AccumulateImp(elapse.count());
+            }
+        }
+
+    private:
+        bool m_stop = false;
+        std::shared_ptr<AccumulatedTimer> m_master = nullptr;
+        std::chrono::time_point<std::chrono::steady_clock> m_start;
+    };
+
+    friend SubTimer;
+    AccumulatedTimer(const AccumulatedTimer &) = delete;
+    AccumulatedTimer & operator= (const AccumulatedTimer &) = delete;
+    
+    ///@brief set time unit of returning timing count
+    static void SetUnit(unit::Time unit)
+    {
+        Instance()->SetUnitImp(unit);
+    }
+
+    ///@brief get time unit of returning timing count
+    static unit::Time GetUnit()
+    {
+        return Instance()->GetUnitImp();
+    }
+
+    ///@brief insert a timer object and start timing immediately
+    ///@note should use a lvalue to hold the returning value explicitly, otherwise the timer will not work
+    static std::unique_ptr<SubTimer> InsertTimer()
+    {    
+        return std::make_unique<SubTimer>(Instance());
+    }
+
+    ///@brief returning current timing count
+    static double Count()
+    {
+        return Instance()->CountImp();
+    }
+    
+    ///@brief reset timing count to zero
+    static void Reset()
+    {
+        Instance()->ResetImp();
+    }
+
+private:
+    AccumulatedTimer() = default;
+
+    static std::shared_ptr<AccumulatedTimer> Instance()
+    {
+        static std::shared_ptr<AccumulatedTimer> timer(new AccumulatedTimer);
+        return timer;
+    }
+
+    void SetUnitImp(unit::Time unit)
+    {
+        m_unit = unit;
+    }
+
+    unit::Time GetUnitImp() const
+    {
+        return m_unit;
+    }
+
+    void ResetImp()
+    {
+        m_count.store(0);
+    }
+
+    void AccumulateImp(int64_t time)
+    {
+        m_count.fetch_add(time);
+    }
+
+    double CountImp() const
+    {
+        double count = m_count.load() * unit::Scale2Second(unit::Time::Nanosecond);
+        double scale = 1.0 / unit::Scale2Second(m_unit);
+        return scale * count;
+    }
+
+private:
+    unit::Time m_unit{unit::Time::Second};
+    std::atomic<int64_t> m_count{0};//nanoseconds;
+};
+
 }//namespace tools
 }//namespace generic
 #endif//GENERIC_TOOLS_HPP
