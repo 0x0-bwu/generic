13002e4577577282d0032c954fd6ba73cc145140
bwu <wbmaxmin@outlook.com>
2023-10-10 15:38:43 +0800

    replace scons with cmake

--- common/Macros.hpp
+++ common/Macros.hpp
@@ -8,9 +8,9 @@
 #pragma once
 #include "Version.hpp"
 
-#define BOOST_SERIALIZATION_SUPPORT 1
+#define BOOST_SERIALIZATION_SUPPORT 0
 #define BOOST_GIL_IO_PNG_SUPPORT    1
-#define EIGEN_LIBRARY_SUPPORT       1
+#define EIGEN_LIBRARY_SUPPORT       0
 #define GENERIC_NO_EXCEPTION        0    
 #define GENERIC_NO_ASSERT           0
 
