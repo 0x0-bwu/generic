f62f9831abdf504a6578d4db6614d7cb8bcf9676
bwu <wbmaxmin@icloud.com>
2023-11-08 23:00:12 +0800

    update ckt module

--- circuit/Simulator.hpp
+++ circuit/Simulator.hpp
@@ -8,120 +8,83 @@
 #pragma once
 
 #include "MNA.hpp"
-
-#if EIGEN_LIBRARY_SUPPORT 
 #include <boost/numeric/odeint.hpp>
-#include <memory>
 #include <set>
 namespace generic::ckt {
 
-using namespace Eigen;
-class CircuitV
+template <typename Float>
+struct DenseCircuit
 {
-public:
-    CircuitV(size_t nodes, std::set<size_t> sources, std::set<size_t> probs)
-     : m_nodes(nodes)
-     , m_probs(std::move(probs))
-     , m_sources(std::move(sources))
+    using VectorType = Eigen::Matrix<Float, Eigen::Dynamic, 1>;
+    using MatrixType = Eigen::Matrix<Float, Eigen::Dynamic, Eigen::Dynamic>;
+    size_t nodes;
+    MatrixType G, C, B, L;
+    std::set<size_t> prob;
+    std::set<size_t> source;
+    DenseCircuit(size_t nodes, std::set<size_t> source, std::set<size_t> prob)
+     : nodes(nodes), prob(prob), source(source)
     {
-        m_G = MatrixXd::Zero(Nodes() + SNodes(), Nodes() + SNodes());
-        m_C = MatrixXd::Zero(Nodes() + SNodes(), Nodes() + SNodes());
-        m_B = MatrixXd::Zero(Nodes() + SNodes(), SNodes());
-        m_L = MatrixXd::Zero(Nodes() + SNodes(), PNodes());
-        
-        size_t i = 0, j = 0;
-        for (auto n : m_sources) {
-            B()(i + Nodes(), i) = -1;
-            mna::StampI(G(), n, i + Nodes());  ++i;
-        }
-        for (auto p : m_probs) {
-            L()(p, j++) = 1;
-        }
-    }
-
-    virtual ~CircuitV() = default;
-
-    void SetR(size_t i, size_t j, double r)
-    {
-        mna::Stamp(G(), i, j, 1 / r);
-    }
-
-    void SetC(size_t i, size_t j, double c)
-    {
-        mna::Stamp(C(), i, j, c);
-    }
-
-    void SetC(size_t i, double c)
-    {
-        mna::Stamp(C(), i, c);
-    }
+        G = MatrixType::Zero(nodes + source.size(), nodes + source.size());
+        C = MatrixType::Zero(nodes + source.size(), nodes + source.size());
+        B = MatrixType::Zero(nodes + source.size(), source.size());
+        L = MatrixType::Zero(nodes + source.size(), prob.size());
     
-    MatrixXd & G() { return m_G; } 
-    MatrixXd & C() { return m_C; } 
-    MatrixXd & B() { return m_B; } 
-    MatrixXd & L() { return m_L; } 
+        size_t i = 0, j = 0;
+        for (auto n : source) {
+            B(i + nodes, i) = -1;
+            mna::StampI(G, n, i + nodes);  ++i;
+        }
+        for (auto p : prob) L(p, j++) = 1;
+    }
 
-    const MatrixXd & G() const { return m_G; } 
-    const MatrixXd & C() const { return m_C; } 
-    const MatrixXd & B() const { return m_B; } 
-    const MatrixXd & L() const { return m_L; } 
-
-    size_t Nodes() const { return m_nodes; }
-    size_t SNodes() const { return Sources().size(); }
-    size_t PNodes() const { return Probs().size(); }
-    const std::set<size_t> & Probs() const { return m_probs; }
-    const std::set<size_t> & Sources() const { return m_sources; }
-
-private:
-    size_t m_nodes;
-    std::set<size_t> m_probs;
-    std::set<size_t> m_sources;
-    MatrixXd m_G, m_C, m_B, m_L;
+    void SetR(size_t i, size_t j, Float r) { mna::Stamp(G, i, j, 1 / r); }
+    void SetC(size_t i, size_t j, Float c) { mna::Stamp(C, i, j, c); }
+    void SetC(size_t i, Float c) { mna::Stamp(C, i, c); }
 };
 
-using StateType = std::vector<double>;
+template <typename Float>
 struct Intermidiate
 {
-    VectorXd u;
-    MatrixXd coeff;
-    MatrixXd input;
-    const CircuitV & ckt;
-    MatrixXd iG, iC, iB, iL;
-    Eigen::PermutationMatrix<Dynamic, Dynamic, size_t> permut;
-    Intermidiate(const CircuitV & ckt, bool verbose = false)
+    using StateType = std::vector<Float>;
+    using VectorType = typename DenseCircuit<Float>::VectorType;
+    using MatrixType = typename DenseCircuit<Float>::MatrixType;
+    using PermutationMatrixType = Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic, size_t>;
+    VectorType u;
+    MatrixType coeff, input;
+    PermutationMatrixType permut;
+    MatrixType iG, iC, iB, iL, iLT;
+    const DenseCircuit<Float> & ckt;
+    Intermidiate(const DenseCircuit<Float> & ckt, bool verbose = false)
      : ckt(ckt)
     {
-        u.resize(ckt.Sources().size());
-        std::tie(iG, iC, iB, iL, permut) = mna::RegularizeSuDynamic(ckt.G(), ckt.C(), ckt.B(), ckt.L(), verbose);
-
+        u.resize(ckt.source.size());
+        std::tie(iG, iC, iB, iL, permut) = mna::RegularizeSuDynamic(ckt.G, ckt.C, ckt.B, ckt.L, verbose);
+        iLT = iL.transpose();
         auto dcomp = iC.ldlt();
-        coeff = dcomp.solve(-1.0 * iG);
+        coeff = dcomp.solve(-1 * iG);
         input = dcomp.solve(iB);
         u.resize(input.cols());
-
         if (verbose) {
-            std::cout << "G:\n" << ckt.G() << std::endl;
-            std::cout << "C:\n" << ckt.C() << std::endl;
-            std::cout << "B:\n" << ckt.B() << std::endl;
-            std::cout << "L:\n" << ckt.L() << std::endl;
-
+            std::cout << "G:\n" << ckt.G << std::endl;
+            std::cout << "C:\n" << ckt.C << std::endl;
+            std::cout << "B:\n" << ckt.B << std::endl;
+            std::cout << "L:\n" << ckt.L << std::endl;
             std::cout << "iG:\n" << iG << std::endl;
             std::cout << "iC:\n" << iC << std::endl;
             std::cout << "iB:\n" << iB << std::endl;
             std::cout << "iL:\n" << iL << std::endl;
-
             std::cout << "coeff:\n" << coeff << std::endl;
             std::cout << "input:\n" << input << std::endl;
-
             std::cout << "p:\n" << permut.indices() << std::endl;
         }
     }
+
     void State2Output(const StateType & x, StateType & result) const
     {
         result.resize(iL.cols());
-        Map<const VectorXd> xvec(x.data(), x.size());
-        Map<VectorXd> ovec(result.data(), result.size());
-        ovec = iL.transpose() * xvec;
+        Eigen::Map<const VectorType> xvec(x.data(), x.size());
+        Eigen::Map<VectorType> ovec(result.data(), result.size());
+        ovec = iLT * xvec;
     }
 
     StateType State2Output(const StateType & x) const
@@ -131,28 +94,25 @@ struct Intermidiate
         return results;
     }
 
-    size_t StateSize() const
-    {
-        return iG.cols();
-    }
+    size_t StateSize() const { return iG.cols(); }
 };
 
-template <typename VoltFunc>
+template <typename Float, typename VoltFunc>
 struct Simulator
 {
-    Intermidiate & im;
     const VoltFunc & vf;
-    Simulator(Intermidiate & im, const VoltFunc & vf) : im(im), vf(vf) {}
+    Intermidiate<Float> & im;
+    using StateType = typename Intermidiate<Float>::StateType;
+    Simulator(Intermidiate<Float> & im, const VoltFunc & vf) : vf(vf), im(im) {}
 
     void operator() (const StateType & x, StateType & dxdt, double t)
     {   
-        for (size_t i = 0; i < im.ckt.SNodes(); ++i) im.u(i) = vf(i, t);
-        Map<const VectorXd> xvec(x.data(), x.size());
-        Map<VectorXd> result(dxdt.data(), dxdt.size());
+        using VectorType = typename  Intermidiate<Float>::VectorType;
+        for (size_t i = 0; i < im.ckt.source.size(); ++i) im.u(i) = vf(i, t);
+        Eigen::Map<VectorType> result(dxdt.data(), dxdt.size());
+        Eigen::Map<const VectorType> xvec(x.data(), x.size());
         result = im.coeff * xvec + im.input * im.u;  
     }
 };
 
 } // namespace generic::ckt
-
-#endif // GENERIC_EIGEN_LIBRARY_SUPPORT
--- test/TestCircuit.hpp
+++ test/TestCircuit.hpp
@@ -11,46 +11,45 @@
 
 using namespace boost::unit_test;
 using namespace generic;
-
-#if EIGEN_LIBRARY_SUPPORT
 using namespace generic::ckt;
+using t_ckt_num_types = boost::mpl::list<double>;
 
-void t_simulator()
+BOOST_TEST_CASE_TEMPLATE_FUNCTION(t_simulator_t, float_t)
 {
-    double vdd = 0.88;
-    double ts  = 200e-12;
+    float_t vdd = 0.88;
+    float_t ts  = 200e-12;
     auto steps = 1000;
-    auto vfun = [vdd, ts](size_t, auto t) { return t > ts ? vdd : t * vdd / ts; };
+    auto vfun = [vdd, ts](size_t, auto t) -> float_t { return t > ts ? vdd : t * vdd / ts; };
 
-    auto ckt = CircuitV(2, {0}, {1});
-    ckt.SetR(0, 1, 1e2);
+    auto ckt = DenseCircuit<float_t>(3, {0}, {1, 2});
+    ckt.SetR(0, 1, 0.01);
+    ckt.SetR(1, 2, 1e2);
     ckt.SetC(1, 1e-12);
     ckt.SetC(2, 1e-12);
 
-    Intermidiate im(ckt, false);
-    StateType initState(im.StateSize(), 0);
-
+    auto im = Intermidiate(ckt, true);
     using namespace boost::numeric;
+    using StateType = typename Intermidiate<float_t>::StateType;
 	using ErrorStepperType = odeint::runge_kutta_cash_karp54<StateType>;
+    
+    StateType initState(im.StateSize(), 0);
 	odeint::integrate_adaptive(
-        odeint::make_controlled(1e-12, 1e-10,ErrorStepperType{}),
-        Simulator(im, vfun), initState, 0.0, ts * 2, ts * 2 / steps);
+        odeint::make_controlled(float_t{1e-12}, float_t{1e-10}, ErrorStepperType{}),
+        Simulator(im, vfun), initState, float_t{0}, float_t{ts * 2}, float_t{ts * 2 / steps});
 
-    std::vector<double> outs;
-    outs = im.State2Output(initState);
+    auto outs = im.State2Output(initState);
+    for (auto out : outs)
+        std::cout << out << ',';
     BOOST_CHECK(outs.size() == 2);
+    BOOST_CHECK_CLOSE(outs.front(), 0.8800, 1e-2);
     BOOST_CHECK_CLOSE(outs.back(), 0.82852, 1e-2);
 }
-
-#endif //EIGEN_LIBRARY_SUPPORT
 
 test_suite * create_circuit_test_suite()
 {
     test_suite * circuit_suite = BOOST_TEST_SUITE("s_circuit");
     //
-#if EIGEN_LIBRARY_SUPPORT
-    circuit_suite->add(BOOST_TEST_CASE(&t_simulator));
-#endif //EIGEN_LIBRARY_SUPPORT
+    circuit_suite->add(BOOST_TEST_CASE_TEMPLATE(t_simulator_t, t_ckt_num_types));
     //
     return circuit_suite;
 }
