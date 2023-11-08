92b07259cc5ddaae18cda4d0338ec555ac8ccb7d
bwu <wbmaxmin@icloud.com>
2023-11-09 01:36:26 +0800

    circuit imp

--- circuit/Simulator.hpp
+++ circuit/Simulator.hpp
@@ -18,28 +18,75 @@ struct DenseCircuit
     using VectorType = Eigen::Matrix<Float, Eigen::Dynamic, 1>;
     using MatrixType = Eigen::Matrix<Float, Eigen::Dynamic, Eigen::Dynamic>;
     size_t nodes;
-    MatrixType G, C, B, L;
+    MNA<MatrixType> m;
     std::set<size_t> prob;
     std::set<size_t> source;
     DenseCircuit(size_t nodes, std::set<size_t> source, std::set<size_t> prob)
      : nodes(nodes), prob(prob), source(source)
     {
-        G = MatrixType::Zero(nodes + source.size(), nodes + source.size());
-        C = MatrixType::Zero(nodes + source.size(), nodes + source.size());
-        B = MatrixType::Zero(nodes + source.size(), source.size());
-        L = MatrixType::Zero(nodes + source.size(), prob.size());
+        m.G = MatrixType::Zero(nodes + source.size(), nodes + source.size());
+        m.C = MatrixType::Zero(nodes + source.size(), nodes + source.size());
+        m.B = MatrixType::Zero(nodes + source.size(), source.size());
+        m.L = MatrixType::Zero(nodes + source.size(), prob.size());
     
         size_t i = 0, j = 0;
         for (auto n : source) {
-            B(i + nodes, i) = -1;
-            mna::StampI(G, n, i + nodes);  ++i;
+            m.B(i + nodes, i) = -1;
+            mna::StampI(m.G, n, i + nodes);  ++i;
         }
-        for (auto p : prob) L(p, j++) = 1;
+        for (auto p : prob) m.L(p, j++) = 1;
     }
 
-    void SetR(size_t i, size_t j, Float r) { mna::Stamp(G, i, j, 1 / r); }
-    void SetC(size_t i, size_t j, Float c) { mna::Stamp(C, i, j, c); }
-    void SetC(size_t i, Float c) { mna::Stamp(C, i, c); }
+    void SetR(size_t i, size_t j, Float r) { mna::Stamp(m.G, i, j, 1 / r); }
+    void SetC(size_t i, size_t j, Float c) { mna::Stamp(m.C, i, j, c); }
+    void SetC(size_t i, Float c) { mna::Stamp(m.C, i, c); }
+    const MNA<MatrixType> & Build() const { return m; }
+};
+
+template <typename Float>
+struct SparseCircuit
+{
+    using MatrixType = Eigen::SparseMatrix<Float>;
+    using Triplets = std::vector<Eigen::Triplet<Float> >;
+    size_t nodes;
+    std::set<size_t> prob;
+    std::set<size_t> source;
+    mutable MNA<MatrixType> m;
+    mutable Triplets tC, tG, tB, tL;
+    SparseCircuit(size_t nodes, std::set<size_t> source, std::set<size_t> prob)
+     : nodes(nodes), prob(prob), source(source)
+    {
+    }
+
+    void SetR(size_t i, size_t j, Float r) { mna::Stamp(tG, i, j, 1 / r); }
+    void SetC(size_t i, size_t j, Float c) { mna::Stamp(tC, i, j, c); }
+    void SetC(size_t i, Float c) { mna::Stamp(tC, i, c); }
+
+    const MNA<MatrixType> & Build() const
+    {
+        m.G = MatrixType(nodes + source.size(), nodes + source.size());
+        m.C = MatrixType(nodes + source.size(), nodes + source.size());
+        m.B = MatrixType(nodes + source.size(), source.size());
+        m.L = MatrixType(nodes + source.size(), prob.size());
+    
+        size_t i = 0, j = 0;
+        for (auto n : source) {
+            tB.emplace_back(i + nodes, i, -1);
+            mna::StampI(tG, n, i + nodes);  ++i;
+        }
+        for (auto p : prob)
+            tL.emplace_back(p, j++, 1);
+
+        m.C.setFromTriplets(tC.begin(), tC.end());
+        m.G.setFromTriplets(tG.begin(), tG.end());
+        m.B.setFromTriplets(tB.begin(), tB.end());
+        m.L.setFromTriplets(tL.begin(), tL.end());
+        tC.clear();
+        tG.clear();
+        tB.clear();
+        tL.clear();
+        return m;
+    }
 };
 
 template <typename Float>
@@ -49,39 +96,40 @@ struct Intermidiate
     using VectorType = typename DenseCircuit<Float>::VectorType;
     using MatrixType = typename DenseCircuit<Float>::MatrixType;
     using PermutationMatrixType = Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic, size_t>;
+    
     VectorType u;
+    MatrixType iLT;
+    size_t stateSize{0};
     MatrixType coeff, input;
-    PermutationMatrixType permut;
-    MatrixType iG, iC, iB, iL, iLT;
-    const DenseCircuit<Float> & ckt;
-    Intermidiate(const DenseCircuit<Float> & ckt, bool verbose = false)
-     : ckt(ckt)
+    const MNA<MatrixType> & m;
+    Intermidiate(const MNA<MatrixType> & m, bool verbose = false)
+     : m(m)
     {
-        u.resize(ckt.source.size());
-        std::tie(iG, iC, iB, iL, permut) = mna::RegularizeSuDynamic(ckt.G, ckt.C, ckt.B, ckt.L, verbose);
-        iLT = iL.transpose();
+        u.resize(m.B.cols());
+        auto [iG, iC, iB, iL] = mna::RegularizeSuDynamic(m, verbose);
         auto dcomp = iC.ldlt();
         coeff = dcomp.solve(-1 * iG);
         input = dcomp.solve(iB);
         u.resize(input.cols());
+        stateSize = iG.cols();
+        iLT = iL.transpose();
         if (verbose) {
-            std::cout << "G:\n" << ckt.G << std::endl;
-            std::cout << "C:\n" << ckt.C << std::endl;
-            std::cout << "B:\n" << ckt.B << std::endl;
-            std::cout << "L:\n" << ckt.L << std::endl;
+            std::cout << "G:\n" << m.G << std::endl;
+            std::cout << "C:\n" << m.C << std::endl;
+            std::cout << "B:\n" << m.B << std::endl;
+            std::cout << "L:\n" << m.L << std::endl;
             std::cout << "iG:\n" << iG << std::endl;
             std::cout << "iC:\n" << iC << std::endl;
             std::cout << "iB:\n" << iB << std::endl;
             std::cout << "iL:\n" << iL << std::endl;
             std::cout << "coeff:\n" << coeff << std::endl;
             std::cout << "input:\n" << input << std::endl;
-            std::cout << "p:\n" << permut.indices() << std::endl;
         }
     }
 
     void State2Output(const StateType & x, StateType & result) const
     {
-        result.resize(iL.cols());
+        result.resize(iLT.rows());
         Eigen::Map<const VectorType> xvec(x.data(), x.size());
         Eigen::Map<VectorType> ovec(result.data(), result.size());
         ovec = iLT * xvec;
@@ -94,7 +142,7 @@ struct Intermidiate
         return results;
     }
 
-    size_t StateSize() const { return iG.cols(); }
+    size_t StateSize() const { return stateSize; }
 };
 
 template <typename Float, typename VoltFunc>
@@ -108,7 +156,7 @@ struct Simulator
     void operator() (const StateType & x, StateType & dxdt, double t)
     {   
         using VectorType = typename  Intermidiate<Float>::VectorType;
-        for (size_t i = 0; i < im.ckt.source.size(); ++i) im.u(i) = vf(i, t);
+        for (size_t i = 0; i < im.m.B.cols(); ++i) im.u(i) = vf(i, t);
         Eigen::Map<VectorType> result(dxdt.data(), dxdt.size());
         Eigen::Map<const VectorType> xvec(x.data(), x.size());
         result = im.coeff * xvec + im.input * im.u;  
