09ac1a606d057b17609c605d1e968cf63390c4e2
0x0-bwu <wbmaxmin@icloud.com>
2024-12-08 15:49:52 +0800

    impl: version utility

--- utils/Version.hpp
+++ utils/Version.hpp
@@ -11,38 +11,40 @@ namespace generic::utils {
 
 struct Version // format: [Major(99).Minor(99).Patch(999)]
 {
-    const std::uint8_t major{0};
-    const std::uint8_t minor{0};
-    const std::uint8_t patch{0};
     Version() = default;
     explicit constexpr Version(std::uint32_t version)
-     : major(version / 100000 % 100), minor(version % 100000 / 1000), patch(version % 1000)
+     : m_major(version / 100000 % 100), m_minor(version % 100000 / 1000), m_patch(version % 1000)
     {
         GENERIC_ASSERT_MSG(version < 10000000, "max version: 99.99.999");
     }
 
     constexpr Version(int major, int minor, int patch)
-     : major(major), minor(minor), patch(patch)
+     : m_major(major), m_minor(minor), m_patch(patch)
     {
-        GENERIC_ASSERT_MSG(major < 100 , "max major version: 99");
-        GENERIC_ASSERT_MSG(minor < 100 , "max minor version: 99");
-        GENERIC_ASSERT_MSG(patch < 1000, "max patch version: 999");
+        GENERIC_ASSERT_MSG(m_major < 100 , "max major version: 99");
+        GENERIC_ASSERT_MSG(m_minor < 100 , "max minor version: 99");
+        GENERIC_ASSERT_MSG(m_patch < 1000, "max patch version: 999");
     }
 
     explicit operator std::uint32_t() const { return toInt(); }
+
     bool operator == (const Version & other) const { return toInt() == other.toInt(); }
     bool operator <  (const Version & other) const { return toInt() <  other.toInt(); }
     bool operator >  (const Version & other) const { return toInt() >  other.toInt(); } 
 
     std::uint32_t toInt() const
     {
-        return major % 100 * 100000 + minor % 100 * 1000 + patch % 1000;
+        return m_major % 100 * 100000 + m_minor % 100 * 1000 + m_patch % 1000;
     }
 
     std::string toString() const
     {
-        return std::to_string(major % 100) + '.' + std::to_string(minor % 100) + '.' + std::to_string(patch % 1000);
+        return std::to_string(m_major % 100) + '.' + std::to_string(m_minor % 100) + '.' + std::to_string(m_patch % 1000);
     }
+private:
+    std::uint8_t m_major{0};
+    std::uint8_t m_minor{0};
+    std::uint8_t m_patch{0};
 };
 
 } // namespace generic::utils
