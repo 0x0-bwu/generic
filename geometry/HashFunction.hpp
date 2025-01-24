3d11c10e6307ae0c32ebf45a37db23236241c8b0
0x0-bwu <wbmaxmin@outlook.com>
2025-01-24 18:15:47 +0800

    impl: hash function

--- geometry/HashFunction.hpp
+++ geometry/HashFunction.hpp
@@ -6,33 +6,16 @@
  * @date 2022-02-22
  */
 #pragma once
-#include "Point.hpp"
-#include <boost/functional/hash.hpp>
-#include <functional>
+#include "generic/tools/Hash.hpp"
+#include "Geometries.hpp"
+
 namespace generic  {
 namespace geometry {
 
 template <typename num_type>
 struct PointHash
 {
-};
-
-template <>
-struct PointHash<int32_t>
-{
-    size_t operator()(const Point2D<int32_t> & point) const noexcept
-    {
-        size_t seed(0);
-        boost::hash_combine(seed, point[0]);
-        boost::hash_combine(seed, point[1]);
-        return seed;
-    }
-};
-
-template <>
-struct PointHash<int64_t>
-{
-    size_t operator()(const Point2D<int64_t> & point) const noexcept
+    size_t operator()(const Point2D<num_type> & point) const noexcept
     {
         size_t seed(0);
         boost::hash_combine(seed, point[0]);
@@ -43,3 +26,119 @@ struct PointHash<int64_t>
 
 }//namespace geometry
 }//namespace generic
+
+
+namespace std {
+
+template <typename num_type>
+struct hash<generic::geometry::Point2D<num_type>>
+{
+    size_t operator() (const generic::geometry::Point2D<num_type> & p) const
+    {
+        size_t seed{0};
+        generic::hash::HashCombine(seed, p[0], p[1]);
+        return seed;
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Point3D<num_type>>
+{
+    size_t operator() (const generic::geometry::Point3D<num_type> & p) const
+    {
+        return generic::hash::HashCombine(0, p[0], p[1], p[2]);
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Segment2D<num_type>>
+{
+    size_t operator() (const generic::geometry::Segment2D<num_type> & s) const
+    {
+        auto hasher = hash<generic::geometry::Point2D<num_type>>();
+        return generic::hash::HashCombine(0, hasher(s[0]), hasher(s[1]));
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Segment3D<num_type>>
+{
+    size_t operator() (const generic::geometry::Segment3D<num_type> & s) const
+    {
+        auto hasher = hash<generic::geometry::Point3D<num_type>>();
+        return generic::hash::HashCombine(0, hasher(s[0]), hasher(s[1]));
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Triangle2D<num_type>>
+{
+    size_t operator() (const generic::geometry::Triangle2D<num_type> & t) const
+    {
+        auto hasher = hash<generic::geometry::Point2D<num_type>>();
+        return generic::hash::HashCombine(0, hasher(t[0]), hasher(t[1]), hasher(t[2]));
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Triangle3D<num_type>>
+{
+    size_t operator() (const generic::geometry::Triangle3D<num_type> & t) const
+    {
+        auto hasher = hash<generic::geometry::Point3D<num_type>>();
+        return generic::hash::HashCombine(0, hasher(t[0]), hasher(t[1]), hasher(t[2]));
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Box2D<num_type>>
+{
+    size_t operator() (const generic::geometry::Box2D<num_type> & b) const
+    {
+        auto hasher = hash<generic::geometry::Point2D<num_type>>();
+        return generic::hash::HashCombine(0, hasher(b[0]), hasher(b[1]));
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Box3D<num_type>>
+{
+    size_t operator() (const generic::geometry::Box3D<num_type> & b) const
+    {
+        auto hasher = hash<generic::geometry::Point3D<num_type>>();
+        return generic::hash::HashCombine(0, hasher(b[0]), hasher(b[1]));
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Polyline2D<num_type>>
+{
+    size_t operator() (const generic::geometry::Polyline2D<num_type> & p) const
+    {
+        auto hasher = hash<generic::geometry::Point2D<num_type>>();
+        return generic::hash::OrderedHash(p, hasher);
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::Polygon2D<num_type>>
+{
+    size_t operator() (const generic::geometry::Polygon2D<num_type> & p) const
+    {
+        auto hasher = hash<generic::geometry::Point2D<num_type>>();
+        return generic::hash::OrderedHash(p.GetPoints(), hasher);
+    }
+};
+
+template <typename num_type>
+struct hash<generic::geometry::PolygonWithHoles2D<num_type>>
+{
+    size_t operator() (const generic::geometry::PolygonWithHoles2D<num_type> & p) const
+    {
+        auto hasher = hash<generic::geometry::Polygon2D<num_type>>();
+        auto holeHash = generic::hash::OrderedHash(p.holes, hasher);
+        return generic::hash::HashCombine(hasher(p.outline), holeHash);
+    }
+};
+
+} // namespace std
