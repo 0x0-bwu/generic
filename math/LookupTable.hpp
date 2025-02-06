bcad93452098d090fec1327a3b33baa6357f1e72
0x0-bwu <350240702.qq.com>
2025-02-07 02:04:08 +0800

    impl: lookup function;

--- math/LookupTable.hpp
+++ math/LookupTable.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "generic/common/Exception.hpp"
+#include "Interpolation.hpp"
 #include <concepts>
 #include <numeric>
 #include <vector>
@@ -63,6 +64,65 @@ public:
     bool isValid() const
     {
         return CalcSize(m_indices) == m_values.size();
+    }
+    
+    Scalar Lookup(Scalar x) const
+    {
+        static_assert(DIM == 1, "only 1D lookup is supported");
+        if (not isValid() or m_values.empty()) {
+            GENERIC_ASSERT_MSG(false, "invalid lookup table");
+            return 0;
+        }
+        if (1 == m_values.size()) return m_values.front();
+        Scalar x1, x2;
+        auto i = InterpIndex(0, x, x1, x2);
+        auto q1 = this->operator()(i), q2 = this->operator()(i + 1);
+        return LinearInterpolation(q1, q2, x1, x2, x);
+    }
+
+    Scalar Lookup(Scalar x, Scalar y) const
+    {
+        static_assert(DIM == 2, "only 2D lookup is supported");
+        if (not isValid() or m_values.empty()) {
+            GENERIC_ASSERT_MSG(false, "invalid lookup table");
+            return 0;
+        }
+        if (1 == m_values.size()) return m_values.front();
+        Scalar x1, x2, y1, y2;
+        auto i1 = InterpIndex(0, x, x1, x2);
+        auto i2 = InterpIndex(1, y, y1, y2);
+        auto q11 = this->operator()(i1, i2);
+        auto q21 = this->operator()(i1 + 1, i2);
+        auto q12 = this->operator()(i1, i2 + 1);
+        auto q22 = this->operator()(i1 + 1, i2 + 1);
+        return BilinearInterpolation(q11, q12, q21, q22, x1, x2, y1, y2, x, y);
+    }
+    
+private:
+    size_t InterpIndex(size_t dim, Scalar x, Scalar & x1, Scalar & x2) const
+    {
+        if (std::isnan(x)) { GENERIC_ASSERT(false); return 0; }
+        if (x < m_indices[dim].front()) {
+            x1 = m_indices[dim][0];
+            x2 = m_indices[dim][1];
+            return 0;
+        }
+        else if (x > m_indices[dim].back()) {
+            x1 = m_indices[dim][m_indices[dim].size() - 2];
+            x2 = m_indices[dim][m_indices[dim].size() - 1];
+            return m_indices[dim].size() - 2;
+        }
+        else {
+            size_t l{0}, r{m_indices[dim].size() - 1}, m;
+            while (r > l + 1) {
+                m = (l + r) / 2;
+                if (m_indices[dim][m] > x) r = m;
+                else l = m;
+            }
+            x1 = m_indices[dim][l];
+            x2 = m_indices[dim][r];
+            return l;
+        }
     }
 
 private:
