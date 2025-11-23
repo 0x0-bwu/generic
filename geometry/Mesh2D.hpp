ec88ea03fe005962eaf4f953339bf7b2efa0c97f
0x0-bwu <350240702@qq.com>
2025-11-23 11:51:00 +0800

    update

--- geometry/Mesh2D.hpp
+++ geometry/Mesh2D.hpp
@@ -11,47 +11,50 @@
 
 namespace generic::geometry::mesh2d {
 
-using coor_t = int64_t;
-using float_t = float_type<coor_t>;
-using IndexEdge = tri::IndexEdge;
-using IndexEdgeList = std::list<IndexEdge>;
-using Point2DContainer = std::vector<Point2D<coor_t> >;
-using PolygonContainer = std::vector<Polygon2D<coor_t> >;
-using Segment2DContainer = std::vector<Segment2D<coor_t> >;
+using Coord = int64_t;
+using Float = float_type<Coord>;
+using Edge = tri::IndexEdge;
+using Edges = std::list<IndexEdge>;
+using Point = Point2D<Coord>;
+using Segment = Segment2D<Coord>
+using Polygon = Polygon2D<Coord>;
+using Points = std::vector<Point>;
+using Segments = std::vector<Segment>;
+using Polygons = std::vector<Polygon>;
 
-inline void ExtractSegment(const Polygon2D<coor_t> & polygon, Segment2DContainer & segments)
+inline void ExtractSegments(const Polygon & polygon, Segments & segments)
 {
     size_t size = polygon.Size();
     for(size_t i = 0; i < size; ++i){
         size_t j = (i + 1) % size;
-        segments.emplace_back(Segment2D<coor_t>(polygon[i], polygon[j]));
+        segments.emplace_back(Segment(polygon[i], polygon[j]));
     }
 }
-
-inline void ExtractSegments(const PolygonContainer & polygons, Segment2DContainer & segments)
+    
+inline void ExtractSegments(const Polygons & polygons, Segments & segments)
 {
-    std::for_each(polygons.begin(), polygons.end(), [&](const auto & p){ ExtractSegment(p, segments); });
+    std::for_each(polygons.begin(), polygons.end(), [&](const auto & p){ ExtractSegments(p, segments); });
 }
 
-inline void ExtractIntersections(const Segment2DContainer & segments, Segment2DContainer & intersections)
+inline void ExtractIntersections(const Segments & segments, Segments & intersections)
 {
     intersections.clear();
     boost::polygon::intersect_segments(intersections, segments.begin(), segments.end());
 }
 
-inline void ExtractTopology(const Segment2DContainer & segments, Point2DContainer & points, IndexEdgeList & edges)
+inline void ExtractTopology(const Segments & segments, Points & points, Edges & edges)
 {
     edges.clear();
     points.clear();
     using EdgeSet = topology::UndirectedIndexEdgeSet;
-    using PointIdxMap = std::unordered_map<Point2D<coor_t>, size_t>;
+    using PointIdxMap = std::unordered_map<Point, size_t>;
     
     EdgeSet edgeSet;
     PointIdxMap pointIdxMap;
 
-    auto getIndex = [&pointIdxMap, &points](const Point2D<coor_t> & p) mutable
+    auto getIndex = [&pointIdxMap, &points](const Point & p) mutable
     {
-        if(!pointIdxMap.count(p)){
+        if (not pointIdxMap.count(p)) {
             pointIdxMap.insert(std::make_pair(p, points.size()));
             points.push_back(p);
         }
@@ -67,12 +70,12 @@ inline void ExtractTopology(const Segment2DContainer & segments, Point2DContaine
     }
 }
 
-inline void MergeClosePointsAndRemapEdge(Point2DContainer & points, IndexEdgeList & edges, coor_t tolerance)
+inline void MergeClosePointsAndRemapEdge(Points & points, Edges & edges, coor_t tolerance)
 {
-    if(tolerance != 0) tri::RemoveDuplicatesAndRemapEdges(points, edges, tolerance);
+    if (tolerance != 0) tri::RemoveDuplicatesAndRemapEdges(points, edges, tolerance);
 }
 
-inline void SplitOverlengthEdges(Point2DContainer & points, IndexEdgeList & edges, coor_t maxLength)
+inline void SplitOverlengthEdges(Points & points, Edges & edges, Coord maxLength)
 {
     if (0 >= maxLength) return;
     auto maxLenSq = maxLength * maxLength;
@@ -99,25 +102,25 @@ inline void SplitOverlengthEdges(Point2DContainer & points, IndexEdgeList & edge
     std::swap(edges, tmp);
 }
 
-inline void AddPointsFromBalancedQuadTree(const Polygon2D<coor_t> & outline, Point2DContainer & points, size_t threshold, size_t threads)
+inline void AddPointsFromBalancedQuadTree(const Polygon & outline, Points & points, size_t threshold, size_t threads)
 {
     struct PointExtent
     {
-        Box2D<coor_t> operator()(const Point2D<coor_t> & point) const
+        Box2D<Coord> operator()(const Point & point) const
         {
-            return Box2D<coor_t>(point, point);
+            return Box2D<Coord>(point, point);
         }
     };
     
-    std::list<Point2D<coor_t> * > objs;
+    std::list<Point * > objs;
     for(size_t i = 0; i < points.size(); ++i)
         objs.push_back(&points[i]);
     
     threshold = std::max(size_t(1), threshold);
-    Box2D<coor_t> bbox = Extent(outline);
-    using Tree = tree::QuadTree<coor_t, Point2D<coor_t>, PointExtent>;
+    Box2D<Coord> bbox = Extent(outline);
+    using Tree = tree::QuadTree<Coord, Point, PointExtent>;
     using Node = typename Tree::QuadNode;
-    using TreeBuilder = tree::QuadTreeBuilderMT<Point2D<coor_t>, Tree>;
+    using TreeBuilder = tree::QuadTreeBuilderMT<Point, Tree>;
     Tree tree(bbox);
     TreeBuilder builder(tree, threads);
     builder.Build(objs, threshold);
@@ -136,12 +139,12 @@ inline void AddPointsFromBalancedQuadTree(const Polygon2D<coor_t> & outline, Poi
     }
 }
 
-inline void TriangulatePointsAndEdges(const Point2DContainer & points, const IndexEdgeList & edges, tri::Triangulation<Point2D<coor_t> > & tri)
+inline void TriangulatePointsAndEdges(const PointContainer & points, const IndexEdgeList & edges, tri::Triangulation<Point> & tri)
 {
     tri.Clear();
     try {
-        tri::Triangulator2D<coor_t> triangulator(tri);
-        triangulator.InsertVertices(points.begin(), points.end(), [](const Point2D<coor_t> & p){ return p[0]; }, [](const Point2D<coor_t> & p){ return p[1]; });
+        tri::Triangulator2D<Coord> triangulator(tri);
+        triangulator.InsertVertices(points.begin(), points.end(), [](const Point & p){ return p[0]; }, [](const Point & p){ return p[1]; });
         triangulator.InsertEdges(edges.begin(), edges.end(), [](const IndexEdge & e){ return e.v1(); }, [](const IndexEdge & e){ return e.v2(); });
         triangulator.EraseOuterTriangles();
     }
